586c21a477d969dc7e05b14906cc2c22208ade7f
ryota19 <dance10113@icloud.com>
2022-04-10 17:58:58 +0900

    route 短縮指定,

---
+++ src/app/Http/Controllers/HelloController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+// global $head, $style, $body, $end;
+// $head  = '<html><head>';
+// $style = <<<EOF
+//         <style>
+//             body { font-size:16pt; color:#999; 
+//             }
+//             h1 { font-size:100pt; text-align:right; color:#eee;
+//                 margin: -40px 0px -50px 0px; }
+//         </style>
+//         EOF;
+// $body = '</head><body>';
+// $end  = '</body></html>';
+
+// function tag($tag, $txt) {
+//     return "<{$tag}>" . $txt . "</{$tag}>";
+// }
+
+class HelloController extends Controller
+{
+    public function __invoke() {
+        return <<<EOF
+        <html>
+        <head>
+        <title>Hello</title>
+        <style>
+            body { font-size:16pt; color:#999
+            }
+            h1 { font-size: 30pt; text-align: right; color:#eee;
+                margin:-15px 0px 0px 0px:}
+        </style>
+        </head>
+        <body>
+            <h1>Single Action</h1>
+            <p>これは、シングルアクションコントローラのアクションです。</p>
+        </body>
+        </html>
+        EOF;
+    }
+}
